5405d60e2e4f0ce665c36f75361fee24230594ab
GGG-qin <771101934@qq.com>
2023-05-15 21:56:43 +0800

    Layout页面搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>首页</div>
+  <LayoutNav />
+  <LayoutHeader />
   <RouterView></RouterView>
+  <LayoutFooter />
 </template>
 
-<script setup></script>
+<script setup>
+  import LayoutNav from "./components/LayoutNav.vue";
+  import LayoutHeader from "./components/LayoutHeader.vue";
+  import LayoutFooter from "./components/LayoutFooter.vue";
+</script>
 
 <style lang="scss" scoped></style>
